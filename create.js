155ae4297a40abed4956e236d8d868bcb4478479
StudyReach-oss <u3209188339@gmail.com>
2026-06-06 18:02:33 +0200

    Update create.js

--- create.js
+++ create.js
@@ -13,6 +13,6 @@ export default async function handler(req, res) {
     const data = await r.json();
     return res.status(200).json({ success: true, study: data[0] });
   } catch (err) {
-    return res.status(500).json({ error: "Erreur serveur" });
+    return res.status(500).json({ error: "Erreur serveur" }); 
   }
 }
